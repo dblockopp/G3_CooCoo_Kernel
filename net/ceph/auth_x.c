6fc8a5ca99e9963735603d826d8b8166afaa2433
Alex Elder <elder@inktank.com>
2012-05-16 15:16:39 -0500

    ceph: messenger: reduce args to create_authorizer
    (cherry picked from commit 74f1869f76d043bad12ec03b4d5f04a8c3d1f157)
    
    Make use of the new ceph_auth_handshake structure in order to reduce
    the number of arguments passed to the create_authorizor method in
    ceph_auth_client_ops.  Use a local variable of that type as a
    shorthand in the get_authorizer method definitions.
    
    Signed-off-by: Alex Elder <elder@inktank.com>
    Reviewed-by: Sage Weil <sage@inktank.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	include/linux/ceph/auth.h
    	net/ceph/auth_x.c
    	net/ceph/osd_client.c

--- net/ceph/auth_x.c
+++ net/ceph/auth_x.c
@@ -620,26 +620,6 @@ static int ceph_x_create_authorizer(
 	return 0;
 }
 
-static int ceph_x_update_authorizer(
-	struct ceph_auth_client *ac, int peer_type,
-	struct ceph_auth_handshake *auth)
-{
-	struct ceph_x_authorizer *au;
-	struct ceph_x_ticket_handler *th;
-
-	th = get_ticket_handler(ac, peer_type);
-	if (IS_ERR(th))
-		return PTR_ERR(th);
-
-	au = (struct ceph_x_authorizer *)auth->authorizer;
-	if (au->secret_id < th->secret_id) {
-		dout("ceph_x_update_authorizer service %u secret %llu < %llu\n",
-		     au->service, au->secret_id, th->secret_id);
-		return ceph_x_build_authorizer(ac, th, au);
-	}
-	return 0;
-}
-
 static int ceph_x_verify_authorizer_reply(struct ceph_auth_client *ac,
 					  struct ceph_authorizer *a, size_t len)
 {
