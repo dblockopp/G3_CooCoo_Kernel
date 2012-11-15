bac2023bbb826415a18684b95ed66fc5c4b2b652
Sarah Sharp <sarah.a.sharp@linux.intel.com>
2012-11-14 17:16:52 -0800

    USB: Increase reset timeout.
    commit 77c7f072c87fa951e9a74805febf26466f31170c upstream.
    
    John's NEC 0.96 xHCI host controller needs a longer timeout for a warm
    reset to complete.  The logs show it takes 650ms to complete the warm
    reset, so extend the hub reset timeout to 800ms to be on the safe side.
    
    This commit should be backported to kernels as old as 3.2, that contain
    the commit 75d7cf72ab9fa01dc70877aa5c68e8ef477229dc "usbcore: refine
    warm reset logic".
    
    Signed-off-by: Sarah Sharp <sarah.a.sharp@linux.intel.com>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Reported-by: John Covici <covici@ccs.covici.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -2249,7 +2249,7 @@ static unsigned hub_is_wusb(struct usb_hub *hub)
 #define HUB_SHORT_RESET_TIME	10
 #define HUB_BH_RESET_TIME	50
 #define HUB_LONG_RESET_TIME	200
-#define HUB_RESET_TIMEOUT	500
+#define HUB_RESET_TIMEOUT	800
 
 /* Is a USB 3.0 port in the Inactive or Complinance Mode state?
  * Port worm reset is required to recover
