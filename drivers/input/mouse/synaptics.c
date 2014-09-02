12272f738e5b358a4b77aa1f829087c212ad85b0
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2014-09-02 09:49:18 -0700

    Input: synaptics - gate forcepad support by DMI check
    commit aa972409951e0675e07918620427517cad5090e0 upstream.
    
    Unfortunately, ForcePad capability is not actually exported over PS/2, so
    we have to resort to DMI checks.
    
    Reported-by: Nicole Faerber <nicole.faerber@kernelconcepts.de>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    [lizf: Backported to 3.4: adjust context]
    Signed-off-by: Zefan Li <lizefan@huawei.com>
    
    Conflicts:
    	drivers/input/mouse/synaptics.c

--- drivers/input/mouse/synaptics.c
+++ drivers/input/mouse/synaptics.c
@@ -497,18 +497,6 @@ static void synaptics_parse_agm(const unsigned char buf[],
 	priv->agm_pending = true;
 }
 
-static void synaptics_parse_ext_buttons(const unsigned char buf[],
-					struct synaptics_data *priv,
-					struct synaptics_hw_state *hw)
-{
-	unsigned int ext_bits =
-		(SYN_CAP_MULTI_BUTTON_NO(priv->ext_cap) + 1) >> 1;
-	unsigned int ext_mask = (1U << ext_bits) - 1;
-
-	hw->ext_buttons = buf[4] & ext_mask;
-	hw->ext_buttons |= (buf[5] & ext_mask) << ext_bits;
-}
-
 static bool is_forcepad;
 
 static int synaptics_parse_hw_state(const unsigned char buf[],
@@ -540,7 +528,7 @@ static int synaptics_parse_hw_state(const unsigned char buf[],
 		hw->left  = (buf[0] & 0x01) ? 1 : 0;
 		hw->right = (buf[0] & 0x02) ? 1 : 0;
 
-		if (SYN_CAP_FORCEPAD(priv->ext_cap_0c)) {
+		if (is_forcepad) {
 			/*
 			 * ForcePads, like Clickpads, use middle button
 			 * bits to report primary button clicks.
