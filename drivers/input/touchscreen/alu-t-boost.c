e6880f7f1815cf415d38600a50d189b4a9597389
Matthew Opperman <mattopp17@gmail.com>
2016-05-10 04:00:53 -0400

    Revert "alu-t-boost: small performance optimization & code Style!"
    This reverts commit ff6351407b7e8e0885e9c5be40ff5f72222b7bc5.

--- drivers/input/touchscreen/alu-t-boost.c
+++ drivers/input/touchscreen/alu-t-boost.c
@@ -63,16 +63,11 @@ module_param(min_input_interval, uint, 0644);
 
 static struct min_cpu_limit {
 	uint32_t user_min_freq_lock[4];
-	uint32_t user_boost_freq_lock[4];
 } limit = {
 	.user_min_freq_lock[0] = 0,
 	.user_min_freq_lock[1] = 0,
 	.user_min_freq_lock[2] = 0,
 	.user_min_freq_lock[3] = 0,
-	.user_boost_freq_lock[0] = 0,
-	.user_boost_freq_lock[1] = 0,
-	.user_boost_freq_lock[2] = 0,
-	.user_boost_freq_lock[3] = 0,
 };
 
 static void do_input_boost_rem(struct work_struct *work)
@@ -80,17 +75,14 @@ static void do_input_boost_rem(struct work_struct *work)
 	unsigned int cpu;
 
 	for_each_possible_cpu(cpu) {
-		if (limit.user_boost_freq_lock[cpu] > 0) {
-			dprintk("Removing input boost for CPU%u\n", cpu);
-			set_cpu_min_lock(cpu, limit.user_min_freq_lock[cpu]);
-			limit.user_boost_freq_lock[cpu] = 0;
-		}
+		dprintk("Removing input boost for CPU%u\n", cpu);
+		set_cpu_min_lock(cpu, limit.user_min_freq_lock[cpu]);
 	}
 }
 
 static void do_input_boost(struct work_struct *work)
 {
-	unsigned int cpu;
+	unsigned int i, cpu;
 	unsigned nr_cpus = nr_boost_cpus;
 
 	cancel_delayed_work_sync(&input_boost_rem);
@@ -100,23 +92,23 @@ static void do_input_boost(struct work_struct *work)
 	else if (nr_cpus > NR_CPUS)
 		nr_cpus = NR_CPUS;
 
-	for (cpu = 0; cpu < nr_cpus; cpu++) {
+	/* Save user current min lock */
+	for_each_possible_cpu(cpu)
+		limit.user_min_freq_lock[cpu] = get_cpu_min_lock(cpu);
+
+	for (i = 0; i < nr_cpus; i++) {
 		struct cpufreq_policy policy;
 		unsigned int cur = 0;
 
-		/* Save user current min & boost lock */
-		limit.user_min_freq_lock[cpu] = get_cpu_min_lock(cpu);
-		limit.user_boost_freq_lock[cpu] = input_boost_freq;
+		dprintk("Input boost for CPU%u\n", i);
+		set_cpu_min_lock(i, input_boost_freq);
 
-		dprintk("Input boost for CPU%u\n", cpu);
-		set_cpu_min_lock(cpu, limit.user_boost_freq_lock[cpu]);
-
-		if (cpu_online(cpu)) {
-			cur = cpufreq_quick_get(cpu);
-			if (cur < limit.user_boost_freq_lock[cpu] && cur > 0) {
-				policy.cpu = cpu;
+		if (cpu_online(i)) {
+			cur = cpufreq_quick_get(i);
+			if (cur < input_boost_freq && cur > 0) {
+				policy.cpu = i;
 				cpufreq_driver_target(&policy,
-					limit.user_boost_freq_lock[cpu], CPUFREQ_RELATION_L);
+					input_boost_freq, CPUFREQ_RELATION_L);
 			}
 		}
 	}
