1237931e71102317647e4daff72cd4bcb98675db
Matthew Opperman <mattopp17@gmail.com>
2016-05-23 02:49:25 -0400

    Revert "msm: Add alucard hotplug"
    This reverts commit 4f0605cd29db5ad7861154bb840f9d27cef9e93f.
    
    Conflicts:
    	arch/arm/mach-msm/Makefile

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -356,9 +356,6 @@ static inline unsigned int cpufreq_quick_get_max(unsigned int cpu)
 }
 #endif
 
-enum {
-	BOOT_CPU = 0,
-};
 
 /*********************************************************************
  *                       CPUFREQ DEFAULT GOVERNOR                    *
