0535d33702faff86e6d8da4bf7b5d8a978e4e908
Matthew Opperman <mattopp17@gmail.com>
2016-07-17 06:27:48 -0400

    Revert "whoops"
    This reverts commit fc865c9d34bfddd45ce87e3e786f869e6477ff6f.

--- include/linux/if_vlan.h
+++ include/linux/if_vlan.h
@@ -122,6 +122,8 @@ static inline u16 vlan_dev_vlan_id(const struct net_device *dev)
 
 static inline bool vlan_do_receive(struct sk_buff **skb, bool last_handler)
 {
+	if (((*skb)->vlan_tci & VLAN_VID_MASK) && last_handler)
+		(*skb)->pkt_type = PACKET_OTHERHOST;
 	return false;
 }
 
